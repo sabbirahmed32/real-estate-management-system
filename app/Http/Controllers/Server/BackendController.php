3da3bed7ba448010ff7e1526b959b60b17b44c1b
sabbirahmed32 <mdsabbirmdjoni@gmail.com>
2026-05-21 22:04:53 +0600

    admin mastaring

---
+++ app/Http/Controllers/Server/BackendController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Server;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BackendController extends Controller
+{
+    //dashboard
+    public function dashboard(){
+        return view('server.dashboard.dashboard-analytics');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\clieant\frontentController;
+use App\Http\Controllers\Server\BackendController;
 
 Route::get('/', [frontentController::class, 'index'])->name('index.data');
 Route::get('/about', [frontentController::class, 'about'])->name('about.data');
@@ -14,3 +15,9 @@ Route::get('/agents/profile', [frontentController::class, 'agentsProfile'])->nam
 Route::get('/blog', [frontentController::class, 'blog'])->name('blog.data');
 Route::get('/blog-details', [frontentController::class, 'blogDetals'])->name('blog.details');
 Route::get('/contact', [frontentController::class, 'contact'])->name('contact.data');
+
+
+
+
+// Server side route
+Route::get('/admin/dashboard/analytics', [BackendController::class, 'dashboard'])->name('dashboard.data');
